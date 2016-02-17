90eef7a096aad0021fcec91a85b2febfdab706ae
vdd9474a <denys.vidal@gmail.com>
2016-02-17 11:28:11 +0100

    implementation algo

--- robot_alimentation.c
+++ robot_alimentation.c
@@ -1,1 +1,29 @@
 #include "robot_alimentation.h"
+
+/* Fonction d'attente de signal
+ */
+void reveive_sig(int sig) {deposer_pc_brt();}
+
+/* Fonction qui deposela piece brut sur le convoyeur
+ */
+void deposer_pc_brt();
+
+void * th_Robot_alimentation()
+{
+	while (1);
+}
+
+void fnc_evenementielle_timerRA()
+{
+	send(defaillance, X);
+	exit("arret du robot d'alimentation, car l'operation de depot de piece a dure plus de 20 sec.");
+}
+
+void deposer_pc_brt()
+{
+	X = sender;
+	armer_timer(timer_RA, 20 sec);
+	deposer_piece_brute();
+	desarmer_timer(timerRA);
+	send(fin_depot_piece_brute_sur_convoyeur, X);
+}
