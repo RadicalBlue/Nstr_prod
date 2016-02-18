c98c2b312d6f269c1f528a0c21d661296b84f453
Blue <blue@Morio>
2016-02-18 14:56:55 +0100

    squelette de th_dialog

--- superviseur.c
+++ superviseur.c
@@ -6,6 +6,15 @@ bool machineEnPanne[NBRMACHINE] = false;
 pthread_mutex_t mutexMachine[NBRMACHINE];
 pthread_mutex_t mutexConvoyeur;
 
+void fnc_evenementielle_USER1()
+{
+  
+}
+
+void fnc_evenementielle_USER2()
+{
+  
+}
 
 void * th_Dialogue()
 {
@@ -22,5 +31,28 @@ void * th_Dialogue()
   Liste listeThreadPiece = creatList();
   /** TODO: sigaction avec un siginfo bien défini pour avoir le pid de celui qui envoie le signal **/
   
+  /** TODO:faire le sigaction **/
   
+  while(1)
+  {
+    saisir_ordre_operateur(code_piece);
+    numero_machine = correspondance_machine_code(code_piece);
+    pthread_mutex_lock(mutexConvoyeur);
+    etat = machineEnPanne[numero_machine];
+    pthread_mutex_unlock(mutexConvoyeur);
+    if(etat == true)
+    {
+      printf("machine demandee en panne\n");
+      while(!isEmpty(listeThreadPiece))
+      {
+	/*attente active*/
+      }
+      erreur("FIN DU SYSTEM : etat defaillance detecte\n",96);
+    }
+    else
+    {
+      new_thread = creer_thread(code_piece,numero_machine);
+      addInList(listeThreadPiece,new_thread);
+    }
+  }
 }
